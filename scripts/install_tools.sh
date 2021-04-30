99c869527d7b4f3deb29b696234ef97d88ab301c
Vaggelis Kappas <vakappas@microsoft.com>
2021-04-30 20:33:34 +0300

    add logging

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -17,7 +17,15 @@ while getopts ":u:p:t:l:" arg; do
     esac
 done
 
+# Create the log file
+log=install_tools_log.txt
+printf "Log File - " > $log
+# append date to log file
+date >> $log
+
+
 # install az cli
+echo "Install Azure CLI ..." >> $log
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 # install docker
@@ -33,10 +41,11 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 # sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 # sudo apt update
 # sudo apt-get install -y kubectl
-
+echo "Install kubectl ..." >> $log
 sudo az aks install-cli
 
 # install helm
+echo "Install helm ..." >> $log
 curl -o helm.tar.gz https://get.helm.sh/helm-v3.3.4-linux-amd64.tar.gz
 tar zxvf helm.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin/helm
@@ -44,6 +53,7 @@ rm -rf linux-amd64
 rm -f helm.tar.gz
 
 # download azdo agent
+echo "Install Azure DevOps agent ..." >> $log
 sudo mkdir -p /opt/azdo && cd /opt/azdo
 cd /opt/azdo
 sudo curl -o azdoagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.184.2/vsts-agent-linux-x64-2.184.2.tar.gz
@@ -51,11 +61,13 @@ sudo tar xzvf azdoagent.tar.gz
 sudo rm -f azdoagent.tar.gz
 
 # configure as azdouser
+echo "Configure Azure DevOps agent ..." >> $log
 sudo chown -R $agentuser /opt/azdo
 sudo chmod -R 755 /opt/azdo
 # runuser -l $agentuser -c "/opt/azdo/config.sh --unattended --url $azdourl --auth pat --token $pat --pool $pool --acceptTeeEula"
-bash /opt/azdo/config.sh --unattended --url $azdourl --auth pat --token $pat --pool $pool --acceptTeeEula
+/opt/azdo/config.sh --unattended --url $azdourl --auth pat --token $pat --pool $pool --acceptTeeEula
 
 # install and start the service
+echo "Configure Azure DevOps agent to run as a service ..." >> $log
 sudo /opt/azdo/svc.sh install
 sudo /opt/azdo/svc.sh start
